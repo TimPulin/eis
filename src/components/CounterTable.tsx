dfc206b6e1635420dad95e963d3a03a3f0a02348
gen <kyliki@yandex.ru>
2024-06-07 09:09:03 +0300

    install axios

---
+++ src/components/CounterTable.tsx
@@ -0,0 +1,23 @@
+export default function CounterTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>№</th>
+          <th>Тип</th>
+          <th>Дата установки</th>
+          <th>Автоматический</th>
+          <th>Текущие показания</th>
+          <th>Адрес</th>
+          <th>Примечание</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
---
+++ src/pages/CounterPage.tsx
@@ -0,0 +1,10 @@
+import CounterTable from '../components/CounterTable';
+
+export default function CounterPage() {
+  return (
+    <>
+      <h1>Список счётчиков</h1>
+      <CounterTable />
+    </>
+  );
+}
